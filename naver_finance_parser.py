03d09b0e8f7c5150adbee72ca199a655132e35c7
임태현(140179) <140179@shinsegae.com>
2016-05-19 17:38:05 +0900

    네이버 증권정보 파서 - 뉴스, 공시 파싱 개발 완료

--- naver_finance_parser.py
+++ naver_finance_parser.py
@@ -1,4 +1,8 @@
 import requests
+from urllib.parse import urljoin
+from scrapy.selector import Selector
+from pprint import pprint
+
 """
 네이버 증권 정보 파서
 종합정보: http://finance.naver.com/item/main.nhn?code=
@@ -10,9 +14,49 @@ import requests
 종목토론실: http://finance.naver.com/item/board.nhn?code=
 """
 
-def get_news(stock_no):
-    url = 'http://finance.naver.com/item/news.nhn?code=' + stock_no
+def fetch_page(url):
     r = requests.get(url)
-    print(r.text)
+    return r.text
 
-get_news('078890')
+def get_news(code, page_limit=3):
+    """
+    네이버 금융 주식 종목 뉴스 파싱
+    기본 1~3페이지까지 파싱
+    """
+    news = []
+    for page in range(1,page_limit+1):
+        url = 'http://finance.naver.com/item/news_news.nhn?code=' + code + '&page=' + str(page)
+        html = fetch_page(url)
+        sel = Selector(text=html)
+        news_list_sel = sel.css("table:not(.Nnavi) tr[align=center]")
+        for news_sel in news_list_sel:
+            date = news_sel.css("td:nth-child(1) span::text").extract()[0]
+            title = news_sel.css("td.title a::text").extract()[0]
+            link = urljoin(url, news_sel.css("td.title a::attr(href)").extract()[0])
+            source = news_sel.css("td:nth-child(3)::text").extract()[0]
+            news.append(dict(date=date, title=title, link=link, source=source))
+    pprint(news)
+    return news
+
+def get_notice(code, page_limit=1):
+    """
+        네이버 금융 공시정보 파싱
+        기본 1 페이지만 파싱
+        """
+    notices = []
+    for page in range(1, page_limit + 1):
+        url = 'http://finance.naver.com/item/news_notice.nhn?code=' + code + '&page=' + str(page)
+        html = fetch_page(url)
+        sel = Selector(text=html)
+        notice_list_sel = sel.css("table:not(.Nnavi) tr[align=center]")
+        for notice_sel in notice_list_sel:
+            date = notice_sel.css("td:nth-child(1) span::text").extract()[0]
+            title = notice_sel.css("td.title a::text").extract()[0]
+            link = urljoin(url, notice_sel.css("td.title a::attr(href)").extract()[0])
+            source = notice_sel.css("td:nth-child(3)::text").extract()[0]
+            notices.append(dict(date=date, title=title, link=link, source=source))
+    pprint(notices)
+    return notices
+
+# get_news('056080', 1)
+# get_notice('078890', 1)
