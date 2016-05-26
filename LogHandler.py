993a00bd726d4031e3470354ff1639746700ed40
임태현(140179) <140179@shinsegae.com>
2016-05-26 18:06:09 +0900

    LogHandler 수정, message 형식 수정

--- LogHandler.py
+++ LogHandler.py
@@ -1,12 +1,23 @@
 import logging
-
+from datetime import datetime
+logging.StreamHandler()
 
 class LogHandler(logging.Handler):
     def __init__(self, view):
         logging.Handler.__init__(self)
         self.view = view
-        self.view.logBrowser.append('LogHandler initiated!')
 
     def emit(self, record):
-        self.view.logBrowser.append(record.message)
+        # print(record.name)
+        # print(record.msecs)
+        # print(record.levelname)
+        # print(record.pathname)
+        # print(record.lineno)
+        # print(record.msg)
+        # print(record.args)
+        # print(record.exc_info)
+        # print(record.getMessage())
+        now = datetime.now()
+        message = "[{level}] {time} - {msg}".format(time=str(now), level=record.levelname, msg=record.msg)
 
+        self.view.logBrowser.append(message)
