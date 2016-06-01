0f5156a9f21da49e35f53017d3ac7145f579f4cf
임태현(140179) <140179@shinsegae.com>
2016-06-01 18:04:15 +0900

    query code 별 in out

---
+++ ebest/temp.py
@@ -0,0 +1,68 @@
+{
+    "t8430": { # 주식종목조회
+        "InBlock": (
+            "gubun"
+        ),
+        "OutBlock": (
+            "hname",
+            "shcode",
+            "expcode",
+            "etfgubun",
+            "uplmtprice",
+            "dnlmtprice",
+            "jnilclose",
+            "memedan",
+            "recprice",
+            "gubun"
+        )
+    },
+    "t8413": { # 주식챠트(일주월)
+        "InBlock": (
+            "shcode",
+            "gubun",
+            "qrycnt",
+            "sdate",
+            "edate",
+            "cts_date",
+            "comp_yn"
+        ),
+        "OutBlock": (
+            "shcode",
+            "jisiga",
+            "jihigh",
+            "jilow",
+            "jiclose",
+            "jivolume",
+            "disiga",
+            "dihigh",
+            "dilow",
+            "diclose",
+            "highend",
+            "lowend",
+            "cts_date",
+            "s_time",
+            "e_time",
+            "dshmin",
+            "rec_count"
+        ),
+        "OutBlock1": (
+            "date",
+            "open",
+            "high",
+            "low",
+            "close",
+            "jdiff_vol",
+            "value",
+            "jongchk",
+            "rate",
+            "pricechk",
+            "ratevalue",
+            "sign"
+        )
+    }
+}
+
+
+
+
+
